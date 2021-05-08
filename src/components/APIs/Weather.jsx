f89018aeb1e3b894beb4b8f7dc09430a08c0840b
Gonzalo Scarlata <gonza.sc.gs@gmail.com>
2021-05-08 11:37:35 -0300

    commit- salgo de esta rama para empezar el sisstema de rutas

--- src/components/APIs/Weather.jsx
+++ src/components/APIs/Weather.jsx
@@ -14,8 +14,8 @@ const Weather = () => {
     };
     var geoOptions = {
         enableHighAccuracy: true,
-        timeout: 27000,
-        maximumAge: 30000,
+        timeout: 1,
+        maximumAge: 0,
     };
 
     const geoSuccess = (position) => {
@@ -23,6 +23,7 @@ const Weather = () => {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude,
         });
+        console.log(position);
         return;
     };
 
@@ -30,70 +31,37 @@ const Weather = () => {
         alert("Hubo un error", error.code);
     };
 
-    /* const gettingPositionsUser = (options) => {
-        if ("geolocation" in navigator) {
-            navigator.geolocation.getCurrentPosition(
-                 (position, error, options ) => {
-                    setCoordenadas({
-                        latitude: position.coords.latitude,
-                        longitude: position.coords.longitude,
-                    });
-                }
-            );
-        } else {
-            console.log("no hay ubic");
-        }
-    }; */
+    
 
     const consultaAPI = async () => {
-        /* gettingPositionsUser(); */
-        //Dummy one, which will result in a working next statement.
-        navigator.geolocation.getCurrentPosition(
-            function () {},
-            function () {},
-            {}
-        );
-        //The working next statement.
-        navigator.geolocation.getCurrentPosition(
-            function (position) {
-                //Your code here
-                setCoordenadas({
-                    latitude: position.coords.latitude,
-                    longitude: position.coords.longitude,
-                });
-            },
-            function (e) {
-                //Your error handling here
-                alert("Hubo un error", e.code);
-            },
-            {
-                enableHighAccuracy: true,
-            }
-        );
-        /* const consulta1 = await fetch(
+        
+        
+        //navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions); 
+        
+        const consulta1 = await fetch(
             "http://api.openweathermap.org/data/2.5/weather?lat=-26.808539&lon=-65.28562649999999&appid=d69142f6fdc12970e9278747e7d64051&units=metric"
-        ); */
-        console.log(coordenadas);
+        );
+        /* console.log(coordenadas);
         const consulta2 = await fetch(
             `api.openweathermap.org/data/2.5/weather?lat=${coordenadas.latitude}&lon=${coordenadas.longitude}&appid=d69142f6fdc12970e9278747e7d64051&units=metric`
-        );
+        ); */
         /* console.log(" consulta", consulta1); */
-        console.log(" consulta2", consulta2);
+        console.log(" consulta2", consulta1);
 
-        if (consulta2.status === 200) {
+        if (consulta1.status === 200) {
             try {
-                /* const respuesta = await consulta.json();
+                const respuesta = await consulta1.json();
                 console.log(
                     "🚀 ~ file: Weather.jsx ~ line 49 ~ consultaAPI ~ respuesta",
                     respuesta
-                ); */
+                );
             } catch (error) {
                 console.log(error);
             }
         }
     };
     consultaAPI();
-    //gettingPositionsUser();
+   
 
     return (
         <div className="city">
