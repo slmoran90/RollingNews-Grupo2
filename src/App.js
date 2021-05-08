5e422860c1d70cd4c8d03bce27ae246eb6306af0
Gonzalo Scarlata <gonza.sc.gs@gmail.com>
2021-05-08 12:22:45 -0300

    Comienzo de maquetado de rutas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
-import './App.css';
+import { Route, Router, Switch } from "react-router";
+import "./App.css";
+import PaginaAdmin from "./components/PaginaAdmin";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Proyecto final RollingNews Grupo 2</h1>
-    </div>
-  );
+    return (
+        <Router>
+            {/*  <Navegacion /> */}
+            {/*Usaremos operador ternario para mostrar barra de navAdmin o NavNormal*/}
+            <Switch>
+                <Route exact path="/">
+                    {/*  <PaginaPrincipal /> */}
+                </Route>
+                
+                {/* Elegir entre las rutas. La barra / es la pagina principal del proyecto (idem index.hml) */}
+                <Route exact path="/administracion">
+                    {/* llamar al componente inicio de administración*/}
+                    <PaginaAdmin />
+                </Route>
+                <Route exact path="/categorias/nueva">
+                    {/*  <NuevaCategoria
+                        consultarAPI={consultarAPI}
+                    ></NuevaCategoria> */}
+                </Route>
+                <Route exact path="/categorias/listar">
+                    {/* <ListarCategoria
+                        categorias={categorias}
+                        consultarAPI={consultarAPI}
+                    ></ListarCategoria> */}
+                </Route>
+                {/* paso como parametro la Categoria. Se pone ? al final si quiero que no sea olbigatorio */}
+                <Route exact path="/noticias/listar/:nombreCategoria">
+                    {/* muestra noticias por categoria */}
+                    {/* <ListarNoticiasxCateg
+                        consultarAPI={consultarAPI}
+                    ></ListarNoticiasxCateg> */}
+                </Route>
+                <Route exact path="/noticias/noticiaCompleta">
+                    {/* <NoticiaCompleta></NoticiaCompleta> */}
+                </Route>
+                <Route path="*">{/* <Error404></Error404> */}</Route>
+            </Switch>
+            {/*  <Footer /> */}
+        </Router>
+    );
 }
 
 export default App;
