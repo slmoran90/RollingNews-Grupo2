8f46c3ba344a63232f89cc4e2e8b65541b046976
Clau DAmichici <clau.damichici@gamil.com>
2021-05-10 20:58:36 -0300

    se agrego maquetado de navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import "./App.css";
+import Navegacion from "./components/common/Navegacion";
+import Footer from "./components/common/Footer";
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Proyecto final RollingNews Grupo 2</h1>
-    </div>
-  );
+    return (
+        <Router>
+            <Navegacion></Navegacion>
+           {/* Navegacion */}
+            {/*Usaremos operador ternario para mostrar barra de navAdmin o NavNormal*/}
+            <Switch>
+                <Route exact path="/">
+                    {/*  <PaginaPrincipal /> */}
+                </Route>
+                <Route>
+                  {/* <Categorias /> */}
+                </Route>
+                <Route>
+                 {/*  <AcercaDeNosotros /> */}
+                </Route>
+                <Route>
+                  {/* <Contacto /> */}
+                </Route>
+                <Route exact path="/categorias/nueva">
+                    {/*  <NuevaCategoria
+                        consultarAPI={consultarAPI}
+                    ></NuevaCategoria> */}
+                </Route>
+                <Route exact path="/categorias/listar">
+                    {/* <ListarCategoria
+                        categorias={categorias}
+                        consultarAPI={consultarAPI}
+                    ></ListarCategoria> */}
+                </Route>
+                {/* paso como parametro la Categoria. Se pone ? al final si quiero que no sea olbigatorio */}
+                <Route exact path="/noticias/listar/:nombreCategoria">
+                    {/* muestra noticias por categoria */}
+                    {/* <ListarNoticiasxCateg
+                        consultarAPI={consultarAPI}
+                    ></ListarNoticiasxCateg> */}
+                </Route>
+                <Route exact path="/noticias/noticiaCompleta">
+                    {/* <NoticiaCompleta></NoticiaCompleta> */}
+                </Route>
+                <Route path="*">{/* <Error404></Error404> */}</Route>
+            </Switch>
+            <Footer></Footer>
+        </Router>
+    );
 }
 
 export default App;
+
---
+++ src/components/common/Navegacion.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import {Navbar, Nav, NavDropdown, Button, Row,Col, Container } from 'react-bootstrap';
+
+const Navegacion = () => {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+      <Row>
+        <Col md={12}>
+        <Navbar.Brand href="/"><img
+        src="/logo_news.png"
+        width="30"
+        height="30"
+        className="d-inline-block align-top"
+        alt="React Bootstrap logo"
+      />Rolling News</Navbar.Brand>
+      </Col>
+      <Col>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link className='nav-link' exact={true}> Inicio </Nav.Link>
+          <Nav.Link className='nav-link' exact={true}> Actualidad </Nav.Link>
+          <Nav.Link className='nav-link' exact={true}> Espectáculos </Nav.Link>
+          <Nav.Link className='nav-link' exact={true}> Tecnología </Nav.Link>
+          <Nav.Link className='nav-link' exact={true}> Deportes </Nav.Link>
+          <NavDropdown title="Más Categorías" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Política</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">
+              Economía
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Salud</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">
+             Fotografía
+            </NavDropdown.Item>
+          </NavDropdown>
+          <NavDropdown title="Admin" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Noticias</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">
+            Categorías
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Nueva noticia</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">
+            Nueva categoría
+            </NavDropdown.Item>
+          </NavDropdown>
+          <Nav.Link className='nav-link' exact={true}>Nosotros</Nav.Link>
+          <Nav.Link className='nav-link' exact={true}>Contacto</Nav.Link>
+          <Button className="ml-5" variant="outline-success">Iniciar Sesión</Button>
+          <Button className="ml-2" variant="outline-success">Registrarse</Button>
+        </Nav>
+      </Navbar.Collapse>
+      </Col>
+      </Row>
+      </Container>
+    </Navbar>
+    
+  );
+};
+
+export default Navegacion;
