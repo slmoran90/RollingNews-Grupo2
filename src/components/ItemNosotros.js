66c4d130452bf3916a946f4fdf055a5e134298ea
Clau DAmichici <clau.damichici@gamil.com>
2021-05-21 23:01:39 -0300

    se creo ItemNosotros

---
+++ src/components/ItemNosotros.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const ItemNosotros = () => {
+    return (
+    //     <Card className='w-100 my-5'>
+    //   <Card.Body>
+    //     <div className="row">
+    //       <div className="col-sm-12 col-md-4">
+    //         <img src={props.personaje.image} alt={props.personaje.character} />
+    //       </div>
+    //       <div className="col-sm-12 col-md-8">
+    //         <Card.Title>{props.personaje.character}</Card.Title>
+    //         <Card.Text>
+    //           {props.personaje.quote}
+    //         </Card.Text>
+    //       </div>
+    //     </div>
+    //   </Card.Body>
+    // </Card>
+    );
+};
+
+export default ItemNosotros;
