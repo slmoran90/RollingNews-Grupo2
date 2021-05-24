72ed32e8c399f3767599af2a323573c6985b727b
Slaya M. Ostapczuk <slaya.ostapczuk@gmail.com>
2021-05-24 17:27:13 -0300

    agregue las primeras validaciones de suscripcion

--- src/components/Suscripcion/Suscripcion.js
+++ src/components/Suscripcion/Suscripcion.js
@@ -1,25 +1,57 @@
-import { React } from "react";
+import React, { useState } from "react";
 import { Form, Col, Button, Container } from "react-bootstrap";
 
 const Suscripcion = () => {
+  const [texto, setTexto] = useState("");
+  const [email, setEmail] = useState("");
+  const [codArea, setCodArea] = useState("");
+  const [error, setError] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (
+      texto.trim() === /^[a-zA-ZÀ-ÿ\s]{3,40}$/ &&
+      email.trim() === /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/ &&
+     codArea ===  /^\d{3,14}$/
+    ) {
+      setError(true);
+      return;
+    } else {
+      setError(false);
+    }
+    e.target.reset();
+  };
+  const formulario = {
+    texto,
+    email,
+    codArea,
+  };
+  console.log(formulario);
+
   return (
     <Container>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group>
           <Form.Label>Nombre y Apellido</Form.Label>
           <Form.Control
             type="text"
             placeholder="Ingrese su Nombre Completo"
+            required
+            onChange={(e) => setTexto(e.target.value)}
           ></Form.Control>
         </Form.Group>
         <Form.Row>
           <Form.Group as={Col}>
             <Form.Label>Dirección</Form.Label>
-            <Form.Control placeholder="Escriba su domicilo"></Form.Control>
+            <Form.Control
+              placeholder="Escriba su domicilo"
+              required
+              onChange={(e) => setTexto(e.target.value)}
+            ></Form.Control>
           </Form.Group>
           <Form.Group as={Col}>
             <Form.Label>Localidad</Form.Label>
-            <Form.Control placeholder="Localidad"></Form.Control>
+            <Form.Control placeholder="Localidad" required onChange={(e) => setTexto(e.target.value)}></Form.Control>
           </Form.Group>
         </Form.Row>
         <Form.Row>
@@ -28,6 +60,8 @@ const Suscripcion = () => {
             <Form.Control
               type="number"
               placeholder="Ingrese su Código Postal"
+              required
+              onChange={(e) => setCodArea(e.target.value)}
             ></Form.Control>
           </Form.Group>
           <Form.Group as={Col}>
@@ -35,14 +69,17 @@ const Suscripcion = () => {
             <Form.Control
               type="number"
               placeholder="Ingrese un número de contacto"
+              required
+              onChange={(e) => setCodArea(e.target.value)}
             ></Form.Control>
           </Form.Group>
         </Form.Row>
-          <Form.Group>
-            <Form.Label>Email</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-          </Form.Group>
-          {/* <Form.Group as={Col}>
+        <Form.Group>
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" required 
+          onChange={(e) => setEmail(e.target.value)}/>
+        </Form.Group>
+        {/* <Form.Group as={Col}>
             <Form.Label>Password</Form.Label>
             <Form.Control type="password" placeholder="Password" />
           </Form.Group> */}
