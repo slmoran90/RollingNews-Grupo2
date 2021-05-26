1156507fcb380c46d95d94dc6dd4f49809c5f354
Clau DAmichici <clau.damichici@gamil.com>
2021-05-26 03:52:09 -0300

    se elimino imagenes de carpeta publica

--- src/components/Error.js
+++ src/components/Error.js
@@ -5,7 +5,7 @@ import "./error.css"
 const Error = () => {
     return (
         <div className="d-flex text-light position-relative">
-            <img className="position-relative w-100" src="/mundo.gif" alt="background"></img>
+            <img className="position-relative w-100" src="/vitals.gif" alt="background"></img>
             <div className="position-absolute cartel m-5">
                 <h1 className="display-3 ">ERROR 404</h1>
                 <h2 className="mt-3">Oops...No se encontro la página</h2>
