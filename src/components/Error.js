eaaeb881e74ccff6de129edb1d478ac8416a762e
Clau DAmichici <clau.damichici@gamil.com>
2021-05-27 13:56:24 -0300

    se modifico button

--- src/components/Error.js
+++ src/components/Error.js
@@ -9,7 +9,7 @@ const Error = () => {
             <div className="position-absolute cartel m-5">
                 <h1 className="display-3 ">ERROR 404</h1>
                 <h2 className="mt-3">Oops...No se encontro la página</h2>
-                <Button href="/" type="button" className="botonvolver m-4">Volver</Button>
+                <Button type="button" className="botonvolver m-4" to="/">Volver</Button>
             </div>
         </div>
     );
